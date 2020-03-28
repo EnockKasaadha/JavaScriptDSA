74686962530f97cfde7ae1ebf9662828f8932b7d
Enock Kasaadha <enocksdha@gmail.com>
2020-03-29 00:11:22 +0300

    added sets data structure notes with some methods

---
+++ sets.js
@@ -0,0 +1,49 @@
+/* 
+    A SET is a collection of items that are unordered and consists of unique elements (meaning they cannot be repeated)
+
+    this data structure uses the same concepts as those of a finite mathematical set but applied to a computer sciece data structure
+
+    //A MATH SET PERSPECTIVE 
+        in mathematics, a set is a collection of disntict objects e.g, a set of natural numbers which can be;
+            N = {0,1,2,3,4,5,6,...}
+            The set list is surrounded by curly braces.
+
+        a set with no element is called a NULL set or EMPTY set
+*/
+
+//IMPLEMENTING A SET
+function Set() {
+    let items = {}; //note that here we use curly  braces not square braces
+
+    /* 
+        //methods that are used include;
+            add(value) - this adds a new item to the set.
+            delete(value) - this removes teh value of the set.
+            has(value) - returns true if a value exists in the set and false otherwise.
+            clear() - removes all items from the list
+            size() - returns how may elements are in the set
+            values() - this returns an array of all the values of the set
+    */
+
+    //the has(value) method
+        //we implement this method first because it is used in other mehods
+        this.has = function(value) {
+            return value in items; //the 'in" verifies that a given value is a property of the items object
+        }
+
+        //OR
+        //a better way to implement this would be
+            this.has = function(value) {
+                return items.hasOwnProperty(value);
+            }
+
+    //the add() method
+    this.add = function(value) {
+        if (!this.has(value)) {
+            items[value] = value;
+            return true;
+        }
+
+        return false;
+    }
+}
